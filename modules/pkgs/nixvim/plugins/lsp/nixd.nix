3cb3479cecc31eeadbba8b77a98e0744dc4d823e
Ladas552 <l.tokshalov@gmail.com>
2025-11-05 15:46:36 +0500

    fixup! make nixd work on nixvim, tho host configuration looks off

--- modules/pkgs/nixvim/plugins/lsp/nixd.nix
+++ modules/pkgs/nixvim/plugins/lsp/nixd.nix
@@ -17,8 +17,8 @@
         settings = {
           nixpkgs.expr = "import <nixpkgs> { }";
           options = {
-            nixos.expr = "(builtins.getFlake ''${config.custom.meta.self}'').nixosConfigurations.NixToks.options";
-            home-manager.expr = "(builtins.getFlake ''${config.custom.meta.self}'').nixosConfigurations.NixToks.options.home-manager.users.type.getSubOptions []";
+            nixos.expr = "(builtins.getFlake ''${config.custom.meta.self}'').nixosConfigurations.NixPort.options";
+            home-manager.expr = "(builtins.getFlake ''${config.custom.meta.self}'').nixosConfigurations.NixPort.options.home-manager.users.type.getSubOptions []";
             nix-on-droid.expr = "(builtins.getFlake ''${config.custom.meta.self}'').nixOnDroidConfigurations.NixMux.options";
             nixvim.expr = "(builtins.getFlake ''${config.custom.meta.self}'').packages.x86_64-linux.nixvim.options";
             nvf.expr = "(builtins.getFlake ''${config.custom.meta.self}'').packages.x86_64-linux.nvf.neovimConfig";
