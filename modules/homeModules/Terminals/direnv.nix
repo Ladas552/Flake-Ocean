47cd347e748588f568defefdd993ddc8a252ea9d
Ladas552 <l.tokshalov@gmail.com>
2025-11-10 11:51:19 +0500

    no direnv allow needed

--- modules/homeModules/Terminals/direnv.nix
+++ modules/homeModules/Terminals/direnv.nix
@@ -11,7 +11,10 @@
             warn_timeout = "0";
             hide_env_diff = true;
           };
-          whitelist.prefix = [ "/home/${config.custom.meta.user}/Projects" ];
+          whitelist.prefix = [
+            "/persist/home/${config.custom.meta.user}/Projects"
+            "/home/${config.custom.meta.user}/Projects"
+          ];
         };
       };
     };
