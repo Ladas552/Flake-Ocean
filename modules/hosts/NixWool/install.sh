17fc4a61a37b8b73d995db481353a5f6c589e176
Ladas552 <l.tokshalov@gmail.com>
2026-02-16 14:45:44 +0500

    finalize the installation hopefully

--- modules/hosts/NixWool/install.sh
+++ modules/hosts/NixWool/install.sh
@@ -2,6 +2,8 @@
 # Stolen from @Jet https://github.com/Michael-C-Buckley/nixos/blob/master/modules/hosts/o1/tools/format.sh
 
 # find a way to put keys for secrets into respective directories yourself
+# My way is to `sudo passwd` a new root password and `ssh root@ip` into the vps
+# Then just `scp ./keys.txt root@ip:/root`
 
 #!/usr/bin/env bash
 set -euo pipefail
@@ -32,7 +34,7 @@ sgdisk --zap-all /dev/sda
 
 echo "Formatting drives..."
 # Put boot on the NVMe then fill the rest with ZFS
-sgdisk -n1:1M:+512M -t1:EF00 -c1:"EFI System" /dev/sda
+sgdisk -n1:1M:+512M -t1:EF00 -c1:"NIXBOOT" /dev/sda
 sgdisk -n2:0:+4G -t2:8200 -c2:"Linux Swap" /dev/sda
 sgdisk -n3:0:0 -t3:BF01 -c3:"ZROOT" /dev/sda
 
@@ -57,6 +59,11 @@ for zvol in "tmp" "nix" "cache" "persist"; do
   zfs create -o mountpoint=legacy zroot/$zvol
   mount -t zfs zroot/$zvol /mnt/$zvol
 done
+
+mkdir -p /mnt/persist/home/ladas552/.ssh
+mkdir -p /mnt/persist/home/ladas552/.config/sops/age
+cp ./NixToks /mnt/persist/home/ladas552/.ssh/
+cp ./keys.txt /mnt/persist/home/ladas552/.config/sops/age/
 
 nixos-install --no-root-password --flake "github:Ladas552/Flake-Ocean#NixWool"
 
