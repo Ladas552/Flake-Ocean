da4547dc9f9d792c405dde9d3a31958900ef2f2a
Ladas552 <l.tokshalov@gmail.com>
2026-02-19 14:01:26 +0500

    a knot.ladas552.me instead of git.ladas552.me

--- modules/nixosModules/Server/tangled.nix
+++ modules/nixosModules/Server/tangled.nix
@@ -24,7 +24,7 @@
             repo.scanPath = "${cfg.stateDir}/repos";
             server = {
               listenAddr = "0.0.0.0:3050";
-              hostname = "git.ladas552.me";
+              hostname = "knot.ladas552.me";
               internalListenAddr = "127.0.0.1:5444";
               owner = "did:plc:6ikdlkw64mrjygj6cea62kn4"; # @ladas552.me
             };
@@ -44,7 +44,7 @@
 
       # Reverse proxy
       services.caddy.virtualHosts = {
-        "git.ladas552.me".extraConfig = ''
+        "knot.ladas552.me".extraConfig = ''
           handle {
             reverse_proxy http://127.0.0.1:3050
           }
